691def69a17ba83ecbfd496a65ca067b36b0d4fe
Reid Hanson <reidhans@buffalo.edu>
2020-01-15 20:53:03 -0500

    make it better

--- .history/gulpfile_20200115202943.js
+++ .history/gulpfile_20200115202943.js
@@ -19,68 +19,6 @@ var argv = require("minimist")(process.argv);
 var gulpif = require("gulp-if");
 var prompt = require("gulp-prompt");
 var rsync = require("gulp-rsync");
-
-gulp.task("deploy", function() {
-  // Dirs and Files to sync
-  rsyncPaths = [
-    path.dist,
-    "lang",
-    "lib",
-    "templates",
-    "./*.php",
-    "./style.css"
-  ];
-
-  // Default options for rsync
-  rsyncConf = {
-    progress: true,
-    incremental: true,
-    relative: true,
-    emptyDirectories: true,
-    recursive: true,
-    clean: true,
-    exclude: []
-  };
-
-  // Staging
-  if (argv.staging) {
-    rsyncConf.hostname = ""; // hostname
-    rsyncConf.username = ""; // ssh username
-    rsyncConf.destination = ""; // path where uploaded files go
-
-    // Production
-  } else if (argv.production) {
-    rsyncConf.hostname = ""; // hostname
-    rsyncConf.username = ""; // ssh username
-    rsyncConf.destination = ""; // path where uploaded files go
-
-    // Missing/Invalid Target
-  } else {
-    throwError("deploy", gutil.colors.red("Missing or invalid target"));
-  }
-
-  // Use gulp-rsync to sync the files
-  return gulp
-    .src(rsyncPaths)
-    .pipe(
-      gulpif(
-        argv.production,
-        prompt.confirm({
-          message: "Heads Up! Are you SURE you want to push to PRODUCTION?",
-          default: false
-        })
-      )
-    )
-    .pipe(rsync(rsyncConf));
-});
-
-function throwError(taskName, msg) {
-  throw new gutil.PluginError({
-    plugin: taskName,
-    message: msg
-  });
-}
-
 // Load package.json for banner
 const pkg = require("./package.json");
 
